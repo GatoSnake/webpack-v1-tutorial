d48874b3967fca20a82b23b51e199400eca36362
Cristhian Won <cristhian.won@gmail.com>
2017-04-02 15:46:08 -0300

    install modules webpack and server dev v1

--- entry.js
+++ entry.js
@@ -1,4 +1,4 @@
 //document.write('It works.')
-//require("!style-loader!css-loader!./style.css");
+//require("!style!css!./style.css");
 require('./style.css');
 document.write(require('./content.js'));
